730e4dfea6b6d7393149eed5f8f38fb968ef9c53
aiswaryass102 <aiswaryass102@gmail.com>
2026-02-11 12:54:49 +0530

    day1

---
+++ da1cw.py
@@ -0,0 +1,13 @@
+apple=15.5
+orange=20
+grape=10.25
+total=apple+orange+grape
+print('total volume sold',total)
+total_volume=int(total)
+print(total_volume)
+a=str(total_volume)
+print('total volume sold',a)
+import random
+rand_num=(random.randrange(5, 10))
+print(rand_num)
+print(rand_num+total_volume)
