640225da838b045e7547a3911495b17b76e6729e
aiswaryass102 <aiswaryass102@gmail.com>
2026-02-17 12:40:47 +0530

    day4

---
+++ day4cw.py
@@ -0,0 +1,16 @@
+
+fruits = ["Apple", "Banana", "Orange"]
+vegetables = ["Carrot", "Potato", "Tomato"]
+beverages = ["Juice", "Soda", "Water"]
+fruits.append("Mango")
+vegetables.insert(1, "Onion")
+beverages.pop()
+inventory = [fruits, vegetables, beverages]
+print("First two fruits:", fruits[:2])
+print("Last vegetable:", vegetables[-1])
+fruit_lengths = [len(item) for item in fruits]
+print("Lengths of fruit names:", fruit_lengths)
+print("Is Water in beverages?", "Water" in beverages)
+first_items = (fruits[0], vegetables[0], beverages[0])
+print("Tuple of first items:", first_items)
+print("Inventory:", inventory)
